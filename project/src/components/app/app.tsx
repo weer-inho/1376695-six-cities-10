73e269c65d69a645786a9a868effbdac5cc42f7a
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-07-12 15:59:28 +0300

    добавил остальные страницы

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,3 +1,6 @@
+// import Login from '../../pages/login/login';
+// import Favorites from '../../pages/favorites/favorites';
+// import Room from '../../pages/room/room';
 import Main from '../../pages/main/main';
 
 type AppProps = {
