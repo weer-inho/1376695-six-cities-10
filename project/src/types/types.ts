db3106051471a654b9292a7b476d77a237c10b84
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-07-18 17:58:33 +0300

    добавил моки

---
+++ project/src/types/types.ts
@@ -0,0 +1,28 @@
+export type ownerType = {
+  avatar: string,
+  name: string,
+  isPro: boolean,
+}
+
+export type offerType = {
+  photos: string[],
+  title: string,
+  description: string;
+  type: string,
+  isPremium: boolean,
+  stars: number,
+  bedrooms: number,
+  guestsMax: number,
+  price: number,
+  householdItems: string[],
+  isFavorite: boolean,
+  ownerInfo: ownerType,
+}
+
+export type reviewType = {
+  avatar: string,
+  name: string,
+  score: number,
+  date: string,
+  reviewText: string,
+}
