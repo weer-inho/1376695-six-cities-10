bbe17383c50bfaecafce76e37cf450f29e4ab5e1
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-08-02 14:26:32 +0300

    добавил редьюсер

---
+++ project/src/store/reducer.ts
@@ -0,0 +1,15 @@
+import {createReducer} from '@reduxjs/toolkit';
+import {offers} from '../mocks/mock-data';
+import {changeCity} from './action';
+
+const initialState = {
+  city: 'Paris',
+  offers: offers,
+};
+
+const reducer = createReducer(initialState, (builder) => {
+  builder.
+    addCase(changeCity, (state, action) => {
+      state.city = action.payload.city;
+  })
+})
