20b008d7d5fe3d71294c49e65566c8f6df208889
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-08-13 14:26:51 +0300

    подключил redux thunk

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -3,11 +3,11 @@ import axios, {AxiosInstance} from 'axios';
 const BACKEND_URL = 'https://10.react.pages.academy/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
-export const createApi= ():AxiosInstance  => {
-    const api = axios.create({
-        baseURL: BACKEND_URL,
-        timeout: REQUEST_TIMEOUT,
-    });
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
 
-    return api;
+  return api;
 };
