0fa8f16a3f4f1e8089db45e31a9906fc7201792d
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-08-18 12:49:18 +0300

    для гостей в шапке главной страницы отображается ссылка перехода на страницу ввода логина-пароля. для авторизованных - информация о пользователе

---
+++ project/src/components/auth-status/auth-status.tsx
@@ -0,0 +1,49 @@
+import {useAppSelector} from '../../hooks';
+import {AppRoot, AuthorizationStatus} from '../../const';
+import {Link} from 'react-router-dom';
+
+function AuthStatus(): JSX.Element {
+  const {authorizationStatus} = useAppSelector((state) => state);
+
+  if (authorizationStatus === AuthorizationStatus.Auth) {
+    return (
+      <ul className='header__nav-list'>
+        <li className='header__nav-item user'>
+          <a
+            className='header__nav-link header__nav-link--profile'
+            href='#xxx'
+          >
+            <div className='header__avatar-wrapper user__avatar-wrapper'></div>
+            <span className='header__user-name user__name'>Oliver.conner@gmail.com</span>
+            <span className='header__favorite-count'>3</span>
+          </a>
+        </li>
+        <li className='header__nav-item'>
+          <a className='header__nav-link' href='#xxx'>
+            <span className='header__signout'>Sign out</span>
+          </a>
+        </li>
+      </ul>
+    );
+  } else {
+    return (
+      <ul className='header__nav-list'>
+        <li className='header__nav-item user'>
+          <a
+            className='header__nav-link header__nav-link--profile'
+            href='#xxx'
+          >
+            <div className='header__avatar-wrapper user__avatar-wrapper'></div>
+          </a>
+        </li>
+        <li className='header__nav-item'>
+          <Link to={AppRoot.Login}>
+            <span className='header__signout'>Sign out</span>
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+}
+
+export default AuthStatus;
